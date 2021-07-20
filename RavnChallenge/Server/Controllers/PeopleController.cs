1d3773026cbfe33ed572ad73beb82973fe8d8e5a
Victor Benavides Arenas <vbenavidesa@inkastudios.com>
2021-07-19 21:09:57 -0500

    BaseController for People
    Created base controller for People

---
+++ RavnChallenge/Server/Controllers/PeopleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RavnChallenge.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        #region Constructor for dependency injection
+        private readonly IConfiguration configuration;
+
+        public PeopleController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        #endregion
+
+        #region public async Task<IActionResult> GetPerson(long id)
+        public async Task<IActionResult> GetPerson(long id)
+        {
+            return Ok();
+        }
+        #endregion
+
+        #region public async Task<IActionResult> GetPeople()
+        public async Task<IActionResult> GetPeople()
+        {
+
+            return Ok();
+        }
+        #endregion
+    }
+}
